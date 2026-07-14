e1f0e90ff111b02408e042e6315748883a3ba2f4
SiliconAves <274307573+SiliconAves@users.noreply.github.com>
2026-07-14 22:09:20 +0200

    Fix: hardcoded version string for ota update

--- scripts/git_branch.py
+++ scripts/git_branch.py
@@ -86,7 +86,7 @@ def inject_version(env):
     base_version = get_base_version(project_dir)
     branch = get_git_branch(project_dir)
     short_sha = get_git_short_sha(project_dir)
-    version_string = f'AvesO3 - 1.3.0.89'
+    version_string = f'1.3.0-89'
     # version_string = f'{base_version}-dev-{branch}-{short_sha}'
 
     env.Append(CPPDEFINES=[('CROSSPOINT_VERSION', f'\\"{version_string}\\"')])
