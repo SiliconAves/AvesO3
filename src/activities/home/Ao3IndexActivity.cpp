ab94d8d643621c8d253f5b40b9bc90d8827bcd52
SiliconAves <274307573+SiliconAves@users.noreply.github.com>
2026-09-14 13:52:39 +0200

    fix: Changed low memory string for Indexing Activity

--- src/activities/home/Ao3IndexActivity.cpp
+++ src/activities/home/Ao3IndexActivity.cpp
@@ -45,7 +45,7 @@ void Ao3IndexActivity::onEnter() {
 void Ao3IndexActivity::runHeapCheck() {
   if (ESP.getMaxAllocHeap() < 48 * 1024) {
     state = State::ERROR;
-    errorMessage = "Insufficient memory to run indexing.";
+    errorMessage = "Not enough memory. Please restart the device.";
     return;
   }
 
